0896b628e22df36cf5095283bcdf7712259fccc3
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-24 16:29:11 -0300

    projeto pronto

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const balls = document.getElementsByClassName('ball');
+const rgbText = document.getElementById('rgb-color');
+const resetBtn = document.getElementById('reset-game');
+const answer = document.getElementById('answer');
+const allBalls = document.getElementById('allBalls');
+const score = document.getElementById('score');
 
 const randomColor = () => {
   const colorsArray = [];
@@ -8,6 +13,24 @@ const randomColor = () => {
     const randomBlue = Math.floor(Math.random() * 256);
     const randomRGB = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
     colorsArray.push((balls[index].style.backgroundColor = randomRGB));
+    rgbText.innerText = colorsArray[0];
   }
 };
+let contador = 0;
+
+allBalls.addEventListener('click', (event) => {
+  if (event.target.style.backgroundColor === rgbText.innerText) {
+    answer.innerText = 'Acertou!';
+    contador += 3;
+    score.innerText = contador;
+  } else {
+    answer.innerText = 'Errou! Tente novamente!';
+  }
+});
+
+resetBtn.addEventListener('click', () => {
+  randomColor();
+  answer.innerText = 'Escolha uma cor';
+});
+
 randomColor();
